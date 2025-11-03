8085cd780331634ddd1f2bf1f63f5f8585c4e71c
Rodrigo Z. A. <contact@arthuso.com>
2025-11-02 21:37:48 -0500

    Refactor place show page

--- app/helpers/places_helper.rb
+++ app/helpers/places_helper.rb
@@ -1,2 +1,39 @@
 module PlacesHelper
+  MAP_DIRECTION_PROVIDERS = [
+    {
+      name: "OpenStreetMap",
+      url: ->(place) { "https://www.openstreetmap.org/directions?from=&to=#{place.lat}%2C#{place.lng}" }
+    },
+    {
+      name: "Google Maps",
+      url: ->(place) { "https://www.google.com/maps/dir/?api=1&destination=#{ERB::Util.url_encode(place.full_address)}" }
+    },
+    {
+      name: "Bing Maps",
+      url: ->(place) { "https://bing.com/maps/default.aspx?rtp=~pos.#{place.lat}_#{place.lng}" }
+    },
+    {
+      name: "Apple Maps",
+      url: ->(place) { "https://maps.apple.com/?daddr=#{place.lat},#{place.lng}" }
+    }
+  ]
+
+  def format_hour(hour_string)
+    return "" if hour_string.blank?
+
+    hour = hour_string.to_i
+    hours = hour / 100
+    minutes = hour % 100
+
+    Time.new(2000, 1, 1, hours, minutes).strftime("%l:%M %P").strip
+  end
+
+  def map_directions_list(place)
+    MAP_DIRECTION_PROVIDERS.map do |provider|
+      {
+        name: provider[:name],
+        url: provider[:url].call(place)
+      }
+    end
+  end
 end
